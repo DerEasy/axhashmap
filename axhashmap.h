909ddf516986a32da4b1034829868e268d840298
DerEasy <alstaden.ismail@gmail.com>
2024-06-09 01:25:54 +0200

    added licence headers

--- axhashmap.h
+++ axhashmap.h
@@ -2,6 +2,12 @@
 // Created by easy on 20.03.24.
 //
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef AXHASH_AXHASHMAP_H
 #define AXHASH_AXHASHMAP_H
 
